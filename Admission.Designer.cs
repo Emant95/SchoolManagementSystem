cc56017c1af7cb60a79d2ee941d01d3472f57600
Hemant Dhakal <D.hemant1995@gmail.com>
2023-08-21 23:20:32 +0545

    Added Admission Form to be modified and Create New student

---
+++ Admission.Designer.cs
@@ -0,0 +1,348 @@
+﻿
+namespace SchoolManagementSystem
+{
+    partial class Admission
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.lbluname = new System.Windows.Forms.Label();
+            this.rbtothers = new System.Windows.Forms.RadioButton();
+            this.rbtfemale = new System.Windows.Forms.RadioButton();
+            this.rbtmale = new System.Windows.Forms.RadioButton();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnAdmit = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbouesrtype = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtphone = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtlname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtfname = new System.Windows.Forms.TextBox();
+            this.lblfname = new System.Windows.Forms.Label();
+            this.studentphoto = new System.Windows.Forms.PictureBox();
+            this.btnupload = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.studentphoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtusername
+            // 
+            this.txtusername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtusername.Location = new System.Drawing.Point(467, 352);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(299, 35);
+            this.txtusername.TabIndex = 51;
+            // 
+            // lbluname
+            // 
+            this.lbluname.AutoSize = true;
+            this.lbluname.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbluname.Location = new System.Drawing.Point(274, 356);
+            this.lbluname.Name = "lbluname";
+            this.lbluname.Size = new System.Drawing.Size(185, 31);
+            this.lbluname.TabIndex = 50;
+            this.lbluname.Text = "Guardian Name";
+            // 
+            // rbtothers
+            // 
+            this.rbtothers.AutoSize = true;
+            this.rbtothers.Location = new System.Drawing.Point(629, 243);
+            this.rbtothers.Name = "rbtothers";
+            this.rbtothers.Size = new System.Drawing.Size(82, 24);
+            this.rbtothers.TabIndex = 49;
+            this.rbtothers.TabStop = true;
+            this.rbtothers.Text = "Others";
+            this.rbtothers.UseVisualStyleBackColor = true;
+            // 
+            // rbtfemale
+            // 
+            this.rbtfemale.AutoSize = true;
+            this.rbtfemale.Location = new System.Drawing.Point(547, 243);
+            this.rbtfemale.Name = "rbtfemale";
+            this.rbtfemale.Size = new System.Drawing.Size(44, 24);
+            this.rbtfemale.TabIndex = 48;
+            this.rbtfemale.TabStop = true;
+            this.rbtfemale.Text = "F";
+            this.rbtfemale.UseVisualStyleBackColor = true;
+            // 
+            // rbtmale
+            // 
+            this.rbtmale.AutoSize = true;
+            this.rbtmale.Location = new System.Drawing.Point(467, 243);
+            this.rbtmale.Name = "rbtmale";
+            this.rbtmale.Size = new System.Drawing.Size(47, 24);
+            this.rbtmale.TabIndex = 47;
+            this.rbtmale.TabStop = true;
+            this.rbtmale.Text = "M";
+            this.rbtmale.UseVisualStyleBackColor = true;
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(641, 698);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(125, 66);
+            this.btnclear.TabIndex = 46;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            // 
+            // btnAdmit
+            // 
+            this.btnAdmit.Location = new System.Drawing.Point(389, 698);
+            this.btnAdmit.Name = "btnAdmit";
+            this.btnAdmit.Size = new System.Drawing.Size(202, 66);
+            this.btnAdmit.TabIndex = 45;
+            this.btnAdmit.Text = "Admit Student";
+            this.btnAdmit.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(265, 528);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 31);
+            this.label7.TabIndex = 43;
+            this.label7.Text = "Photo";
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpassword.Location = new System.Drawing.Point(467, 466);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '*';
+            this.txtpassword.Size = new System.Drawing.Size(299, 35);
+            this.txtpassword.TabIndex = 42;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(274, 470);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 31);
+            this.label6.TabIndex = 41;
+            this.label6.Text = "Address";
+            // 
+            // txtemail
+            // 
+            this.txtemail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtemail.Location = new System.Drawing.Point(467, 407);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(299, 35);
+            this.txtemail.TabIndex = 40;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(274, 411);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 31);
+            this.label5.TabIndex = 39;
+            this.label5.Text = "Email";
+            // 
+            // cbouesrtype
+            // 
+            this.cbouesrtype.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbouesrtype.FormattingEnabled = true;
+            this.cbouesrtype.Items.AddRange(new object[] {
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine",
+            "Ten"});
+            this.cbouesrtype.Location = new System.Drawing.Point(467, 293);
+            this.cbouesrtype.Name = "cbouesrtype";
+            this.cbouesrtype.Size = new System.Drawing.Size(299, 37);
+            this.cbouesrtype.TabIndex = 38;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(274, 299);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 31);
+            this.label4.TabIndex = 37;
+            this.label4.Text = "Class";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(274, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 31);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "Gender";
+            // 
+            // txtphone
+            // 
+            this.txtphone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtphone.Location = new System.Drawing.Point(467, 170);
+            this.txtphone.MaxLength = 14;
+            this.txtphone.Name = "txtphone";
+            this.txtphone.Size = new System.Drawing.Size(299, 35);
+            this.txtphone.TabIndex = 35;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(274, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 31);
+            this.label2.TabIndex = 34;
+            this.label2.Text = "Guardian Phone";
+            // 
+            // txtlname
+            // 
+            this.txtlname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtlname.Location = new System.Drawing.Point(467, 104);
+            this.txtlname.Name = "txtlname";
+            this.txtlname.Size = new System.Drawing.Size(299, 35);
+            this.txtlname.TabIndex = 33;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(274, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 31);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "Last Name";
+            // 
+            // txtfname
+            // 
+            this.txtfname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtfname.Location = new System.Drawing.Point(467, 41);
+            this.txtfname.Name = "txtfname";
+            this.txtfname.Size = new System.Drawing.Size(299, 35);
+            this.txtfname.TabIndex = 31;
+            // 
+            // lblfname
+            // 
+            this.lblfname.AutoSize = true;
+            this.lblfname.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfname.Location = new System.Drawing.Point(274, 45);
+            this.lblfname.Name = "lblfname";
+            this.lblfname.Size = new System.Drawing.Size(142, 31);
+            this.lblfname.TabIndex = 30;
+            this.lblfname.Text = "First Name";
+            // 
+            // studentphoto
+            // 
+            this.studentphoto.Location = new System.Drawing.Point(467, 543);
+            this.studentphoto.Name = "studentphoto";
+            this.studentphoto.Size = new System.Drawing.Size(299, 126);
+            this.studentphoto.TabIndex = 52;
+            this.studentphoto.TabStop = false;
+            // 
+            // btnupload
+            // 
+            this.btnupload.Location = new System.Drawing.Point(850, 562);
+            this.btnupload.Name = "btnupload";
+            this.btnupload.Size = new System.Drawing.Size(125, 66);
+            this.btnupload.TabIndex = 53;
+            this.btnupload.Text = "Sign Up";
+            this.btnupload.UseVisualStyleBackColor = true;
+            // 
+            // Admission
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 816);
+            this.Controls.Add(this.btnupload);
+            this.Controls.Add(this.studentphoto);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.lbluname);
+            this.Controls.Add(this.rbtothers);
+            this.Controls.Add(this.rbtfemale);
+            this.Controls.Add(this.rbtmale);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnAdmit);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbouesrtype);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtphone);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtlname);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtfname);
+            this.Controls.Add(this.lblfname);
+            this.Name = "Admission";
+            this.Text = "Admission";
+            ((System.ComponentModel.ISupportInitialize)(this.studentphoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.Label lbluname;
+        private System.Windows.Forms.RadioButton rbtothers;
+        private System.Windows.Forms.RadioButton rbtfemale;
+        private System.Windows.Forms.RadioButton rbtmale;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btnAdmit;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbouesrtype;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtphone;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtlname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtfname;
+        private System.Windows.Forms.Label lblfname;
+        private System.Windows.Forms.PictureBox studentphoto;
+        private System.Windows.Forms.Button btnupload;
+    }
+}
